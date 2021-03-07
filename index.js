f3ff595052844b349e9f1bbf7828a4af2445945f
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-03-07 16:59:36 +0530

    socketio code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 
 // locol imports
 const router = require('./router');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const PORT = process.env.PORT || 8000;
 
@@ -16,10 +17,26 @@ const io = socketio(server);
 
 // io functions
 io.on('connection', (socket) => {
-	console.log('we have a new connection!!!');
-
 	socket.on('join', ({ name, room }, callback) => {
-		console.log(name, room);
+		const { error, user } = addUser({ id: socket.id, name, room });
+		if (error) {
+			return callback(error);
+		}
+		socket.emit('message', {
+			user: 'admin',
+			text: `${user.name}, welcome to the room ${user.room}`,
+		});
+		socket.broadcast
+			.to(user.room)
+			.emit('message', { user: 'admin', text: `${user.name} has joined the room` });
+		socket.join(user.room);
+		callback();
+	});
+
+	socket.on('send message', (message, callback) => {
+		const user = getUser(socket.id);
+		io.to(user.room).emit('message', { user: user.name, text: message });
+		callback();
 	});
 
 	socket.on('disconnect', () => {
